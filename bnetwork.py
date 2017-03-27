ed0da5163bc185df8c0f514319323709ebfda718
Pavan Joshi <pavan01121994@gmail.com>
2017-03-27 12:15:49 -0400

    Added API to handle inference queries and evaluation metrics Also added API to get independencies using pgmpy

--- bnetwork.py
+++ bnetwork.py
@@ -14,7 +14,8 @@
 #	24-Mar-2017     Anurag Dixit	Changes for Linear Regression intercept
 #	24-Mar-2017		Pavan Joshi		Adding API to Create a Hybrid Bayesian Network using libpgm
 #	25-Mar-2017     Anurag Dixit	Added API for calculation of entropy and KL Divergence
-#	26-Mar-2017 	Pavan Joshi		
+#	26-Mar-2017 	Pavan Joshi		Added API to get independencies in the model
+#	27-Mar-2017		Pavan Joshi		Added API to handle inference queries and evaluation metrics
 #
 ################################################################################
 import os
@@ -23,6 +24,7 @@ import json
 import numpy as np
 import pandas as pd
 
+from pgmpy.models import BayesianModel
 from libpgm.hybayesiannetwork import HyBayesianNetwork
 from libpgm.nodedata import NodeData
 from libpgm.graphskeleton import GraphSkeleton
@@ -42,166 +44,8 @@ class BNetwork(Ndata):
 		Ndata.__init__(self, fname)
 		self.fname = fname
 		self.model = Structure()
+		self.independencies_model = BayesianModel()
 
-
-	def sdalinit__(self,nodeName,parents = [],continuous = False):
-		"""
-		Parameters
-		----------
-		nodeName: The name of the node in the bayesian network
-
-		parents	: The parents of the node.
-				  Defaut value = []. Signifies no parents
-
-		continuous: Signifies whether the node contains continuous values
-				  False - Discrete
-				  True - Continuous
-				  Default value = False(Discrete)
-
-		Return
-		------
-		None
-
-		Examples
-		--------
-
-		>>> x = Node('X')
-		>>> y = Node('Y',parents=['X'])
-		>>> z = Node('Z',parents=['X','Y'],continuous=True)
-
-		"""
-		self.nodeName = nodeName
-		self.parents = np.array(parents)
-		self.continuous = continuous
-		self.cpd = None
-
-	def add_parent(self,parent):
-		self.parents = np.append(self.parents,parent)
-
-	def add_cpd(self,node_values,parent_values):
-		"""
-		Parameters
-		----------
-
-		node_values		: Values that the node takes.
-						  This is the discrete values or a variance in case
-						  of continuous distribution.
-
-		parent_values	: The parent values
-						  Takes a list of number of discrete parent values.
-						  Or it takes the beta vector which specifies the
-						  mean of the node as a function of the parent nodes.
-						  (Includes beta_0, the constant term which is specified
-						  at the beginning)
-
-		Return
-		------
-		None
-
-		Examples
-		--------
-
-		>>> x = Node('X')
-		>>> x.add_cpd([[0.3, 0.05, 0.9, 0.5], [0.4, 0.25, 0.08, 0.3],
-		 									[0.3, 0.7, 0.02, 0.2]], [2,2])
-		CPD added successfully
-
-		>>> y = Node('Y',continuous=True)
-		>>> y.add_cpd(9.6 , [0.2,-2, 3, 7])
-		CPD added successfully
-
-		"""
-		if self.continuous:
-			if(parent_values.shape[0]!=(self.parents.shape[0]+1)):
-				print "Error: Dimension Mismatch"
-				return
-			self.cpd = LinearGaussianCPD(
-							self.nodeName,
-							parent_values,
-							node_values,
-							self.parents)
-		else:
-			variable_card = np.array(node_values).shape[0]
-			if(variable_card==1):
-				variable_card = np.array(node_values).shape[1]
-			if(len(parent_values) != 0 and
-					np.prod(parent_values)!=np.array(node_values).shape[1]):
-				print "Error: Dimension Mismatch"
-				return
-			self.cpd = TabularCPD(
-							self.nodeName,
-							variable_card,
-							node_values,
-							self.parents,
-							parent_values
-							)
-
-		print('CPD added successfully')
-
-	def get_cpd(self):
-		"""
-		Parameters
-		----------
-		None
-
-		Return
-		------
-		Conditional Probability Distribution
-
-		Examples
-		--------
-
-		>>> x = Node('X',parents=['Y','Z'])
-		>>> x.add_cpd([[0.3, 0.05, 0.9, 0.5], [0.4, 0.25, 0.08, 0.3],
-		 									[0.3, 0.7, 0.02, 0.2]], [2,2])
-		>>> x.get_cpd()
-
-
-		"""
-		return self.cpd
-
-	def get_edges(self):
-		"""
-		Parameters
-		----------
-		None
-
-		Return
-		------
-		List of edges in the form (parent,child)
-
-		Examples
-		--------
-
-		>>> x = Node('X',parents=['Y','Z'])
-		>>> x.get_edges()
-		[('Y','X'),('Z','X')]
-
-		"""
-		edges = []
-		for parent in self.parents:
-			edges.append((parent,self.nodeName))
-		return edges
-
-	def get_name(self):
-		"""
-		Parameters
-		----------
-		None
-
-		Return
-		------
-		String Name
-
-		Examples
-		--------
-
-		>>> g = Node('Grade',parents=['Difficulty','Intelligence'])
-		>>> g.get_name()
-		'Grade'
-
-		"""
-		return self.nodeName
 
 	def define_structure(self):
 
@@ -214,7 +58,6 @@ class BNetwork(Ndata):
 		self.model.add_edge(['postLength','postShareCt'],types=['lg','lg'])
 		self.model.add_edge(['postLength','Comments'],types=['lg','lgandd'])
 		self.model.add_edge(['postShareCt','Comments'],types=['lg','lgandd'])
-		#self.model.add_edge(['baseTime','cc1'],types=['lg','lg'])
 		self.model.add_edge(['baseDay','cc2'],types=['d','lgandd'])
 		self.model.add_edge(['cc1','cc2'],types=['lg','lgandd'])
 		self.model.add_edge(['cc2','cc3'],types=['lgandd','lg'])
@@ -222,6 +65,21 @@ class BNetwork(Ndata):
 		self.model.add_edge(['pageCheckins','Comments'],types=['lg','lgandd'])
 		self.model.add_edge(['postDay','cc4'],types=['d','lgandd'])
 		self.model.add_edge(['cc4','Comments'],types=['lgandd','lgandd'])
+
+		self.independencies_model.add_edge('pageCategory','pagePopularity')
+		self.independencies_model.add_edge('pagePopularity','pageTalkingAbout')
+		self.independencies_model.add_edge('pageTalkingAbout','Comments')
+		self.independencies_model.add_edge('postPromotion','Comments')
+		self.independencies_model.add_edge('postLength','postShareCt')
+		self.independencies_model.add_edge('postLength','Comments')
+		self.independencies_model.add_edge('postShareCt','Comments')
+		self.independencies_model.add_edge('baseDay','cc2')
+		self.independencies_model.add_edge('cc1','cc2')
+		self.independencies_model.add_edge('cc2','cc3')
+		self.independencies_model.add_edge('cc3','Comments')
+		self.independencies_model.add_edge('pageCheckins','Comments')
+		self.independencies_model.add_edge('postDay','cc4')
+		self.independencies_model.add_edge('cc4','Comments')
 
 		with open("structure.json","wb") as json_file:
 			json_file.write(self.model.get_structure())
@@ -257,19 +115,59 @@ class BNetwork(Ndata):
 		with open("nodedata.json","wb") as json_file:
 			json_file.write(json.dumps(self.node_data, indent=2))
 
+	def get_independencies(self,variables=None):
+		if variables == None:
+			return self.independencies_model.get_independencies()
+		return self.independencies_model.local_independencies(variables)
+
+	def probability_query(self,query,evidence = {}):
+		prob = 1
+		if len(evidence.keys()) == 0:
+			samples = self.bayesian_network.randomsample(1000)
+		else:
+			samples = self.bayesian_network.randomsample(1000,evidence)
+		aggregate = self.aggregator.aggregate(samples)
+		for key in query.keys():
+			valDict = aggregate[key]
+			if(self.model.get_vertex_type(key)!="d"):
+				values = np.array(valDict.keys(),dtype=np.float)
+				dist = scipy.stats.norm(np.mean(values),np.var(values))
+				prob *= dist.pdf(query[key])
+			else:
+				prob *= float(valDict[str(query[key])])
+		return prob
+
 
 	def infer(self):
-		#infr = Mplp(self.model)
 		f = open('query.txt', 'r')
 		lines = f.readlines()
 		for i in lines:
-			lst = i.split(",")
+			lst = i.strip().split(", ")
 			queryType = lst[0]
-			if(queryType == 0):
-				child = lst[1]
-				parents = lst[2:]
-
-			#print infr.query(child, parents)
+			if(queryType == 'I'):
+				print "\n\n##########    Printing all independencies    ##########\n"
+				print self.get_independencies()
+			elif(queryType == 'LI'):
+				var = lst[1].strip("[").strip("]").split(" & ")
+				print "\n\n#########    Local independencies for",var,"   #########\n"
+				print self.get_independencies(variables = var)
+			elif(queryType == 'CP'):
+				evidences = dict()
+				query = dict()
+				args = lst[1].split("=")
+				query[args[0].strip().strip(" ")] = float(args[1].strip().strip(" "))
+				args = lst[2].split(" -> ")
+				for arg in args[1].strip("[").strip("]").split("&"):
+					evid = arg.strip().strip(" ").split("=")
+					evidences[evid[0].strip().strip(" ")] = float(evid[1].strip().strip(" "))
+				print "\n\n##########    P(",lst[1],"|",lst[2].split(" -> ")[1],")    ##########\n"
+				print self.probability_query(query,evidences)
+			elif(queryType == 'M'):
+				query = dict()
+				args = lst[1].split("=")
+				query[args[0].strip().strip(" ")] = float(args[1].strip().strip(" "))
+				print "\n\n##########    P(",lst[1],")    ##########\n"
+				print self.probability_query(query)
 
 		#TODO: Add handling of multiple types of queries defined in query file
 
@@ -292,7 +190,21 @@ class BNetwork(Ndata):
 		ndata.entriestoinstances()
 
 		self.bayesian_network = HyBayesianNetwork(skeleton,ndata)
-		result = self.bayesian_network.randomsample(100)
+		self.aggregator = SampleAggregator()
+
+	def evaluation_metrics(self):
+		result = self.bayesian_network.randomsample(1000)
+		aggregate = self.aggregator.aggregate(result)['Comments']
+		samples = np.array(aggregate.keys(),dtype=np.float)
+		samplepdf = scipy.stats.norm(np.mean(samples),np.var(samples))
+		origin_data = np.array(self.target,dtype=np.float)
+		print origin_data
+		originalpdf = scipy.stats.norm(np.mean(origin_data),np.var(origin_data))
+		query_domain = np.linspace(np.mean(origin_data) - np.var(origin_data),
+		 						np.mean(origin_data) + np.var(origin_data), 100)
+		entropy, kl_divergence = self.metric(samplepdf.pdf(query_domain),originalpdf.pdf(query_domain))
+		print "Entropy:",entropy
+		print "KL Divergence:",kl_divergence
 
 
 if __name__=="__main__":
@@ -304,10 +216,8 @@ if __name__=="__main__":
 
 			fname.append(dirname + files)
 
-	#ob = Data(fname)
 	bn = BNetwork(fname)
 	bn.define_structure()
 	bn.create_network()
-	#dat = pd.DataFrame(ob.data, columns = ['pagePopularity', 'pageCheckins', 'pageTalkingAbt',  'pageCategory', 'cc1', 'cc2', 'cc3', 'cc4', 'cc5','baseTime', 'postLength','postShareCt', 'postPromotion', 'hLocal', 'postDay' , 'Comments'])
-
 	bn.infer()
+	bn.evaluation_metrics()
